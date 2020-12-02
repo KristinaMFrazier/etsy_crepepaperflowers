9d4f046bde645ec0d4a643239e5575d9d437f41a
Kristina Frazier <kristinafrazier@Kristinas-MBP.fios-router.home>
2020-12-02 18:10:20 -0500

    Refactor: Update Dataclean_int.ipynb
    Update Dataclean_int.ipynb to make all tables lower case and fix
    error to properly explode style and taxonomy_path tables

--- tables.sql
+++ tables.sql
@@ -1,27 +1,28 @@
--- LISTINGS_MAIN
+-- CREATE LISTINGS_MAIN
 
 CREATE TABLE public.listings_main_11282020
 (
-    main_id integer,
-    listing_id "char",
-    user_id "char",
-    title "char",
-    description "char",
-    price,
-    currency_code "char",
-    quantity,
-    url "char",
-    views,
-    num_favorers,
-    who_made "char",
-    when_made "char",
-    item_weight_unit,
-    recipient,
-    occasion,
-    taxonomy_path,
-    is_vintage
+    main_id numeric,
+    listing_id text,
+    user_id text,
+    title text,
+    description text,
+    price double precision,
+    currency_code text,
+    quantity numeric,
+    url text,
+    views numeric,
+    num_favorers numeric,
+    who_made text,
+    when_made text,
+    recipient text,
+    occasion text,
+    is_vintage text,
     PRIMARY KEY (main_id)
 );
 
 ALTER TABLE public.listings_main_11282020
     OWNER to postgres;
+
+-- IMPORT LISTINGS_MAIN
+\copy listings_main_11282020 (main_id,listing_id,user_id,title,description,price,currency_code,quantity,url,views,num_favorers,who_made,when_made,recipient,occasion,is_vintage) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_main.csv' delimiter ',' csv header;
