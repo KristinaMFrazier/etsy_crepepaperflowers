bf10570405d4c755792d15f682ccad204ecccdf9
Kristina Frazier <kristinafrazier@Kristinas-MBP.fios-router.home>
2020-12-02 19:46:30 -0500

    Chore: Add PostgreSQL data upload code

--- tables.sql
+++ tables.sql
@@ -1,3 +1,5 @@
+-- Table creation and import codes for data uploads
+
 -- CREATE LISTINGS_MAIN
 
 CREATE TABLE public.listings_main_11282020
@@ -17,8 +19,7 @@ CREATE TABLE public.listings_main_11282020
     when_made text,
     recipient text,
     occasion text,
-    is_vintage text,
-    PRIMARY KEY (main_id)
+    is_vintage text
 );
 
 ALTER TABLE public.listings_main_11282020
@@ -26,3 +27,71 @@ ALTER TABLE public.listings_main_11282020
 
 -- IMPORT LISTINGS_MAIN
 \copy listings_main_11282020 (main_id,listing_id,user_id,title,description,price,currency_code,quantity,url,views,num_favorers,who_made,when_made,recipient,occasion,is_vintage) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_main.csv' delimiter ',' csv header;
+
+--------------------------------------------------------------------------------
+
+-- CREATE LISTINGS_TAGS
+
+CREATE TABLE public.listings_tags_11282020
+(
+    main_id numeric,
+    tags text
+);
+
+ALTER TABLE public.listings_tags_11282020
+    OWNER to postgres;
+
+
+-- IMPORT LISTINGS_TAGS
+\copy listings_tags_11282020 (main_id,tags) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_tags.csv' delimiter ',' csv header;
+
+--------------------------------------------------------------------------------
+
+-- CREATE LISTINGS_MATERIALS
+
+CREATE TABLE public.listings_materials_11282020
+(
+    main_id numeric,
+    materials text
+);
+
+ALTER TABLE public.listings_materials_11282020
+    OWNER to postgres;
+
+
+-- IMPORT LISTINGS_MATERIALS
+\copy listings_materials_11282020 (main_id,materials) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_materials.csv' delimiter ',' csv header;
+
+--------------------------------------------------------------------------------
+
+-- CREATE LISTINGS_STYLE
+
+CREATE TABLE public.listings_style_11282020
+(
+    main_id numeric,
+    style text
+);
+
+ALTER TABLE public.listings_style_11282020
+    OWNER to postgres;
+
+
+-- IMPORT LISTINGS_STYLE
+\copy listings_style_11282020 (main_id,style) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_style.csv' delimiter ',' csv header;
+
+--------------------------------------------------------------------------------
+
+-- CREATE LISTINGS_TAXONOMY
+
+CREATE TABLE public.listings_taxonomy_11282020
+(
+    main_id numeric,
+    taxonomy_path text
+);
+
+ALTER TABLE public.listings_taxonomy_11282020
+    OWNER to postgres;
+
+
+-- IMPORT LISTINGS_TAXONOMY
+\copy listings_taxonomy_11282020 (main_id,taxonomy_path) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_taxonomy.csv' delimiter ',' csv header;
