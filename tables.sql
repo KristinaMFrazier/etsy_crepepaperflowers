1490c227cd0dacc287a8b895f6f3771bceee0adb
Kristina Frazier <kristinafrazier@Kristinas-MBP.fios-router.home>
2020-12-23 22:08:46 -0500

    Refactor: Update tables.sql table ids
    The listing_id field is now the id to link all tables. Update PostGreSQL
    table creation commands with new field.

--- tables.sql
+++ tables.sql
@@ -2,9 +2,8 @@
 
 -- CREATE LISTINGS_MAIN
 
-CREATE TABLE public.listings_main_11282020
+CREATE TABLE public.listings_main_12232020
 (
-    main_id numeric,
     listing_id text,
     user_id text,
     title text,
@@ -22,76 +21,76 @@ CREATE TABLE public.listings_main_11282020
     is_vintage text
 );
 
-ALTER TABLE public.listings_main_11282020
+ALTER TABLE public.listings_main_12232020
     OWNER to postgres;
 
 -- IMPORT LISTINGS_MAIN
-\copy listings_main_11282020 (main_id,listing_id,user_id,title,description,price,currency_code,quantity,url,views,num_favorers,who_made,when_made,recipient,occasion,is_vintage) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_main.csv' delimiter ',' csv header;
+\copy listings_main_11282020 (listing_id,user_id,title,description,price,currency_code,quantity,url,views,num_favorers,who_made,when_made,recipient,occasion,is_vintage) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_main.csv' delimiter ',' csv header;
 
 --------------------------------------------------------------------------------
 
 -- CREATE LISTINGS_TAGS
 
-CREATE TABLE public.listings_tags_11282020
+CREATE TABLE public.listings_tags_12232020
 (
-    main_id numeric,
+    listing_id text,
     tags text
 );
 
-ALTER TABLE public.listings_tags_11282020
+ALTER TABLE public.listings_tags_12232020
     OWNER to postgres;
 
 
 -- IMPORT LISTINGS_TAGS
-\copy listings_tags_11282020 (main_id,tags) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_tags.csv' delimiter ',' csv header;
+\copy listings_tags_12232020 (listing_id,tags) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_tags.csv' delimiter ',' csv header;
 
 --------------------------------------------------------------------------------
 
 -- CREATE LISTINGS_MATERIALS
 
-CREATE TABLE public.listings_materials_11282020
+CREATE TABLE public.listings_materials_12232020
 (
-    main_id numeric,
+    listing_id text,
     materials text
 );
 
-ALTER TABLE public.listings_materials_11282020
+ALTER TABLE public.listings_materials_12232020
     OWNER to postgres;
 
 
 -- IMPORT LISTINGS_MATERIALS
-\copy listings_materials_11282020 (main_id,materials) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_materials.csv' delimiter ',' csv header;
+\copy listings_materials_12232020 (listing_id,materials) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_materials.csv' delimiter ',' csv header;
 
 --------------------------------------------------------------------------------
 
 -- CREATE LISTINGS_STYLE
 
-CREATE TABLE public.listings_style_11282020
+CREATE TABLE public.listings_style_12232020
 (
-    main_id numeric,
+    listing_id text,
     style text
 );
 
-ALTER TABLE public.listings_style_11282020
+ALTER TABLE public.listings_style_12232020
     OWNER to postgres;
 
 
 -- IMPORT LISTINGS_STYLE
-\copy listings_style_11282020 (main_id,style) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_style.csv' delimiter ',' csv header;
+\copy listings_style_12232020 (listing_id,style) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_style.csv' delimiter ',' csv header;
 
 --------------------------------------------------------------------------------
 
 -- CREATE LISTINGS_TAXONOMY
 
-CREATE TABLE public.listings_taxonomy_11282020
+CREATE TABLE public.listings_taxonomy_12232020
 (
-    main_id numeric,
+    listing_id text,
     taxonomy_path text
 );
 
-ALTER TABLE public.listings_taxonomy_11282020
+ALTER TABLE public.listings_taxonomy_12232020
     OWNER to postgres;
 
 
 -- IMPORT LISTINGS_TAXONOMY
-\copy listings_taxonomy_11282020 (main_id,taxonomy_path) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_taxonomy.csv' delimiter ',' csv header;
+\copy listings_taxonomy_12232020 (listing_id,taxonomy_path) from '/Users/kristinafrazier/documents/projects/etsy/data/nov_2020/listings_taxonomy.csv' delimiter ',' csv header;
